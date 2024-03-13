3600a4d73c49ca7d1a1888154f5e1c79757cf34d
Hsharghi841 <153832695+Hsharghi841@users.noreply.github.com>
2024-03-13 13:21:35 +0330

    isPasswordValid added to Utils

--- src/UtilityPackage/Utils.java
+++ src/UtilityPackage/Utils.java
@@ -1,4 +1,19 @@
 package UtilityPackage;
 
 public class Utils {
+    public static boolean isPasswordValid(String pass){
+        boolean let = false, num = false;
+        for (int i = 0; i < pass.length(); i++) {
+            if(!let)
+                if(pass.charAt(i) <= 'z' && pass.charAt(i) >= 'a' || pass.charAt(i) >= 'A' && pass.charAt(i) <= 'Z')
+                    let = true;
+            else if(!num) {
+                if (pass.charAt(i) >= '0' && pass.charAt(i) <= '9')
+                    num = true;
+            }else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
